c5fcc6e6b1bfa8fea39eed15ac342b85baf16faa
maoHangFei <3061148499@qq.com>
2024-02-26 10:56:45 +0800

    a

--- src/views/dataScreen/leftComp.vue
+++ src/views/dataScreen/leftComp.vue
@@ -2,7 +2,9 @@
   <div class="boxContent">
     <div class="boxContent-public">
       <div class="boxContent-public-title">职位分布情况</div>
-      <div class="boxContent-public-main"></div>
+      <div class="boxContent-public-main">
+        <div id="lBox1"></div>
+      </div>
     </div>
 
     <div class="boxContent-public">
@@ -13,14 +15,125 @@
 </template>
 
 <script lang="ts">
-import { Component, Prop, Vue, Watch } from "vue-property-decorator";
+import {Component, Prop, Vue, Watch} from "vue-property-decorator";
 
 @Component({
   components: {},
 })
-export default class leftComp extends Vue {}
+export default class leftComp extends Vue {
+  lBox1Data: any = {
+    xData: [],
+    yData: [],
+  }
+
+  getLBox1Data() {
+  }
+
+  initLBox1() {
+    let option: any = {
+      tooltip: {
+        trigger: "axis",
+      },
+      grid: {
+        top: "14%",
+        bottom: "15%",
+      },
+      xAxis: [
+        {
+          type: "category",
+          data: ["2019-01", "2019-02", "2019-03", "2019-04", "2019-05", "2019-06"],
+          axisLine: {
+            lineStyle: {
+              color: "#fff",
+            },
+          },
+        },
+      ],
+      yAxis: [
+        {
+          type: "value",
+          splitNumber: 4,
+          splitLine: {
+            show: false,
+            lineStyle: {
+              type: "dashed",
+              color: "#DDD",
+            },
+          },
+          axisLine: {
+            show: false,
+            lineStyle: {
+              color: "#fff",
+            },
+          },
+          nameTextStyle: {
+            color: "#999",
+          },
+          splitArea: {
+            show: false,
+          },
+        },
+      ],
+      series: [
+        {
+          name: "课时",
+          type: "line",
+          data: [23, 60, 20, 36, 23, 85],
+          lineStyle: {
+            normal: {
+              width: 2, // 线条粗细
+              color: {
+                type: "linear",
+
+                colorStops: [
+                  {
+                    offset: 0,
+                    color: "#A9F387", // 0% 处的颜色
+                  },
+                  {
+                    offset: 1,
+                    color: "#48D8BF", // 100% 处的颜色
+                  },
+                ],
+                globalCoord: false, // 缺省为 false
+              },
+              shadowColor: "rgba(72,216,191, 0.3)",
+              shadowBlur: 10,
+              shadowOffsetY: 20,
+            },
+          },
+          itemStyle: {
+            normal: {
+              color: "#48D8BF",
+              borderWidth: 10,
+            },
+          },
+          smooth: true,
+        },
+      ],
+    };
+
+    let myChart = (this as any).$echarts.init(
+        document.getElementById("lBox1")
+    ); // 图标初始化
+    myChart.setOption(option); // 渲染页面
+    /* ECharts动态效果 */
+    window.addEventListener("resize", () => {
+      myChart.resize();
+    });
+  }
+
+  mounted() {
+    this.initLBox1()
+  }
+}
 </script>
 
 <style lang="scss" scoped>
 @import "publicStyle";
+
+#lBox1 {
+  width: 100%;
+  height: 100%;
+}
 </style>
